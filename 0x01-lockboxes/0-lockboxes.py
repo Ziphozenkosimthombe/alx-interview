df102db5813a34a3b80fca32b0ac5219362ed9a6
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-17 13:04:05 +0000

    alx-interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,17 +4,25 @@
 
 def canUnlockAll(boxes):
     """
-    a method that determines if all the boxes can be opened.
-
-    :param boxes:
-    :return: True or False
+    # check if boxes is a list
+    # if the box not a list return False
+    # check if the list is empty
+    # loop through the keys boxes
+    # loop through the boxes
+    # check if the box is not in the keys,
+    # list and the box is less than the length of the boxes 
+    #append the box to the keys list
+    # check if the length of the keys,
+    # boxes is equal to the length of the boxes
+    # return True
+    # return False
     """
     if not boxes or type(boxes) is not list:
         return False
 
     keys = [0]
     for key in keys:
-        for box in boxes[n]:
+        for box in boxes[key]:
             if box not in keys and box < len(boxes):
                 keys.append(box)
     if len(keys) == len(boxes):
