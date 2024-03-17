a1ad3e7a9b2545a673167d630586cd2eae6ed08d
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-17 12:15:10 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+
+
 def canUnlockAll(boxes):
-    """ Method that determines if all boxes can be opened """
-
-    for key in range(1, len(boxes)):
-        flag = False
-        for box in range(len(boxes)):
-            if key in boxes[box] and box != key:
-                flag = True
-                break
-        if not flag:
-            return False
-
-    return True
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
