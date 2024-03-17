47465bfab978ad0f4b0c0ef54098af7f91ac2e11
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-17 13:00:05 +0000

    alx-interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,18 +6,17 @@ def canUnlockAll(boxes):
     """
     # check if boxes is a list
     # if the box not a list return False
-     # check if the list is empty
-     # loop through the keys boxes
-     # loop through the boxes
-     # check if the box is not in the keys,
-     # list and the box is less than the length of the boxes 
-     #append the box to the keys list
-     # check if the length of the keys,
-     # boxes is equal to the length of the boxes
-     # return True
-     # return False
+    # check if the list is empty
+    # loop through the keys boxes
+    # loop through the boxes
+    # check if the box is not in the keys,
+    # list and the box is less than the length of the boxes
+    # append the box to the keys list
+    # check if the length of the keys,
+    # boxes is equal to the length of the boxes
+    # return True
+    # return False
     """
-    
     if not boxes or type(boxes) is not list:
         return False
 
