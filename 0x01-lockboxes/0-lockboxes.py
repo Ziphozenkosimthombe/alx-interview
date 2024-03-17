ef0f5d08148e879be90d5b2eee63cca001281130
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-17 13:54:12 +0200

    update changes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python3
+"""
+1. # If boxes is not a list, return False
+1. # Check if boxes is a list
+2. # Check if the first box is empty
+3. # Check if the key is in the boxes
+4. # Check if the box is not already in the keys
+5. # Check if the box is less than the length of the boxes
+6. # Add the box to the keys
+7. # Check if the length of the keys is equal to the length of the boxes
+8. # Return True if the length of the keys is equal to the length of the boxes
+9. # Return False if the length of the keys is not equal to the length of the boxes
+10. # Return False if boxes is not a list
+"""
+
 def canUnlockAll(boxes):
-    # Check if boxes is a list
     if not boxes:
-        # If boxes is not a list, return False
         return False
-    # Check if boxes is a list of lists
     keys = [0]
-    # Check if the first box is empty
     for key in keys:
-        # Check if the key is in the boxes
         for box in boxes[key]:
-            # Check if the box is not already in the keys
-            # Check if the box is less than the length of the boxes
             if box not in keys and box < len(boxes):
-                # Add the box to the keys
                 keys.append(box)
-    # Check if the length of the keys is equal to the length of the boxes
     return len(keys) == len(boxes)
