41e8a4eccefdbf47fad248a492f74a42f92a63c5
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-18 06:16:23 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,7 +10,7 @@ def canUnlockAll(boxes):
     # loop through the keys boxes
     # loop through the boxes
     # check if the box is not in the keys,
-    # list and the box is less than the length of the boxes 
+    # list and the box is less than the length of the boxes
     #append the box to the keys list
     # check if the length of the keys,
     # boxes is equal to the length of the boxes
